0f13c53c85bb561d1c10a0c3a1c5bdb409f80c08
Robin Prak <stickabush@gmail.com>
2017-03-27 00:21:02 +0200

    Attempting a Collection class

--- collection.py
+++ collection.py
@@ -3,11 +3,11 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
-class Collection:
 
-    ## URL from Debat Gemist
+class Collection:
+    # URL from Debat Gemist
     collectionurl = "http://debatgemist.tweedekamer.nl"
-    ## Search query
+    # Search query
     refattribute = "bekijk dit debat"
 
     # Retrieve HTML
@@ -15,7 +15,7 @@ class Collection:
         html = urlopen(Collection.collectionurl)
         return html
 
-    ## Iterate links and return list
+    # Iterate links and return list
     def getLinks():
         html = Collection.getHTML()
         bsObj = BeautifulSoup(html, "lxml")
@@ -29,21 +29,16 @@ class Collection:
         return refList
 
 
-## links = Collection.getLinks("http://debatgemist.tweedekamer.nl", "bekijk dit debat")
+# links = Collection.getLinks("http://debatgemist.tweedekamer.nl", "bekijk dit debat")
 links = Collection.getLinks()
 
-
 print(len(links))
-## Output: 11
+# Output: 11
 
 print(links)
 
-# # Output: ['/debatten/stemmingen-403',Collection  '/debatten/geannoteerde-agenda-europese-raad-24-en-25-maart',
+# Output: ['/debatten/stemmingen-403',Collection  '/debatten/geannoteerde-agenda-europese-raad-24-en-25-maart',
 # '/debatten/regeling-van-werkzaamheden-659', '/debatten/vaststelling-profielschets-nieuwe-voorzitter',
 # '/debatten/be%C3%ABdiging-leden', '/debatten/be%C3%ABdiging-tijdelijke-voorzitter', '/debatten/mededelingen-726',
 # '/debatten/afscheid-vertrekkende-leden-0', '/debatten/ontwerpprofielschets-nieuwe-voorzitter',
 # '/debatten/commissie-geloofsbrieven', '/debatten/mededelingen-725']
-
-
-
-
